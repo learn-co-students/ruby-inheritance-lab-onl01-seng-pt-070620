5b8df791ec5e2fa410e2d9250610c6e3b1900032
Andy Adams <andrewdcadams@gmail.com>
2020-08-06 23:40:43 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User 
+  
+  attr_accessor :knowledge 
+  
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(k_string)
+    @knowledge << k_string
+  end
+  
 
 end

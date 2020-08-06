857e4293f2310d92270837c3a6be909c42139eb0
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-08-06 06:42:39 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+  def initialize
+    @knowledge=Array.new
+  end
+
+  def learn(neww)
+    @knowledge<<neww
+  end
+  
+  def knowledge
+    @knowledge
+  end
 end

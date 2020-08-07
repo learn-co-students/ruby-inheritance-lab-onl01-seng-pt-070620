e51d165ca339af07e3334981ad27b4687981c5c2
Sean Ransonette <sransonette@gmail.com>
2020-08-07 03:04:49 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,30 @@
-class Student 
+require 'pry'
+
+class Student < User
+  
+  attr_accessor :first_name, :last_name  
+  
+  
+    
+  
+  def initialize 
+    @first_name = first_name 
+    @last_name = last_name
+    @knowledge = []
+    
+
+  end
+  
+  def learn(knowledge)
+    @knowledge << "Ruby framework Rails gem bundle update"
+  end 
+  
+  def knowledge 
+    @knowledge << "Javascript Ember Elixir knowledge"
+    #binding.pry
+    @knowledge
+  end 
+
+
 
 end
